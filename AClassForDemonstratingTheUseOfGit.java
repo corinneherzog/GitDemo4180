2d1bb8e6e4a77eec19cb7e68aa4a07e1f581c78d
corinneherzog <Corinneh2001@gmail.com>
2018-05-20 11:47:04 -0700

    Changed to French for main, added a method in Spanish

--- AClassForDemonstratingTheUseOfGit.java
+++ AClassForDemonstratingTheUseOfGit.java
@@ -1,9 +1,13 @@
 public class AClassForDemonstratingTheUseOfGit {
-    public static void main(String[] los_args) {
-        diHola("git");
+    public static void main(String[] le_args) {
+        sayHello("git");
     }
 
+    public static void sayHello(String le_toWhom) {
+        System.out.println("Bonjour, " + le_toWhom + "!");
+    }
+    
     public static void diHola(String aQuien) {
-        System.out.println("Hola, " + aQuien + "!");
+        System.out.println("Hola, " + aQuien + "!");    
     }
 }
