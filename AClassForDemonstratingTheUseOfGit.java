f51891804ac123db5ea6926650255304bceb85c6
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-05-20 04:27:43 -0700

    Refactored to use API internally

--- AClassForDemonstratingTheUseOfGit.java
+++ AClassForDemonstratingTheUseOfGit.java
@@ -1,6 +1,6 @@
 public class AClassForDemonstratingTheUseOfGit {
     public static void main(String[] args) {
-        System.out.println("Hello, git!");
+        sayHello("git");
     }
 
     public static void sayHello(String toWhom) {
